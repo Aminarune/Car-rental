9f36a3a6e1cea68edc986211f4e0362c97144a2e
Hibiniy <mifels.fem@gmail.com>
2023-01-04 15:18:41 +0700

    Edit config to use svg in storybook

--- frontend/.storybook/main.cjs
+++ frontend/.storybook/main.cjs
@@ -1,18 +1,21 @@
+const svgrRollup = require("@svgr/rollup");
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-vite",
   },
-  "features": {
-    "storyStoreV7": true
-  }
-}
+  features: {
+    storyStoreV7: true,
+  },
+  viteFinal: (config) => {
+    config.plugins = [...config.plugins, svgrRollup()];
+    return config;
+  },
+};
